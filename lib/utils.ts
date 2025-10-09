f9ff4b63ca13620637b850d25235224daac19889
Kizito Chiazor <kizitochiazor@gmail.com>
2025-10-09 23:06:53 +0100

    Implement dwolla and plaid functionality

--- lib/utils.ts
+++ lib/utils.ts
@@ -202,7 +202,7 @@ export const authFormSchema = (type: string) =>
     lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
     address1: type === "sign-in" ? z.string().optional() : z.string().min(3),
     city: type === "sign-in" ? z.string().optional() : z.string().min(3),
-    state: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    state: type === "sign-in" ? z.string().optional() : z.string().min(1),
     postalCode: type === "sign-in" ? z.string().optional() : z.string().min(3),
     dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
     ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
